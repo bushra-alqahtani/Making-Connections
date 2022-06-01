0d92b36205f67239aa9da38e6de54f45408f326c
Bushra AlQahtani <89680878+bushra-alqahtani@users.noreply.github.com>
2022-06-02 01:18:44 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,30 @@
+var imgelemnt = document.querySelector("#hide");
+
+var count= connection.innerText;
+
+function hide(e){
+   
+   imgelemnt.remove();
+   count--;
+   connection.innerText= count;
+   
+}
+
+var imgelemnt2 = document.querySelector("#hide2");
+
+var count2= connection.innerText;
+
+
+function hide2(e){
+   count--;
+   imgelemnt2.remove();
+   connection.innerText= count;
+   
+}
+
+
+
+function edit(){
+
+    name1.innerHTML="Asma";
+}
